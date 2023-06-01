43c813ae1f152cbd0669b065ecb3e7e48f4dc335
Dmitrii Mazurok <tmazurok@gmail.com>
2023-06-01 21:52:30 +0300

    add import path alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
 import { defineConfig } from "vite";
+import url from "url";
 import react from "@vitejs/plugin-react-swc";
 import ssr from "vite-plugin-ssr/plugin";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), ssr()],
+  resolve: {
+    alias: {
+      "#root": url.fileURLToPath(new URL(".", import.meta.url)),
+    },
+  },
 });
