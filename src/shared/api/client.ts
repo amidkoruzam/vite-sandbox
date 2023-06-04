6ef07f4d75fed5b88b14403bbdeaa6ee27223115
Dmitrii Mazurok <tmazurok@gmail.com>
2023-06-04 19:20:56 +0300

    tmp fix for node-fetch running at client

--- src/shared/api/client.ts
+++ src/shared/api/client.ts
@@ -1,3 +1,1 @@
-import serverFetch from "node-fetch";
-
-export const client = typeof window === "undefined" ? serverFetch : fetch; // should probably set fetch as global
+export const client = fetch; // should probably set fetch as global
